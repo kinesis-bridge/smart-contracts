c576107a412ee184a7004436fb340cbc74106ce1
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2026-04-27 18:07:59 +0200

    Add KIA Oracle deployment scripts

--- shell/gen_target.sh
+++ shell/gen_target.sh
@@ -7,11 +7,12 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 SRC_DIR=pact
 TARGET_DIR=pact_target
 NAMESPACE=n_e595727b657fbbb3b8e362a05a7bb8d12865c1ff
+KIA_NAMESPACE=n_40c883decc192e1e3214898f04656b2e9ea7b74e
 
 ## Update the namespace in target_dir
 
 find $SRC_DIR -type f -name "*.pact" | while read f; do
   out="$TARGET_DIR/${f#$SRC_DIR/}"
   mkdir -p "$(dirname "$out")"
-  m4 -DNAMESPACE=$NAMESPACE <(echo 'changequote([[, ]])') "$f" > "$out"
+  m4 -DNAMESPACE=$NAMESPACE -DKIA_NAMESPACE=$KIA_NAMESPACE <(echo 'changequote([[, ]])') "$f" > "$out"
 done
